db3ad622874e3c994d66732595a5fe1788acdb68
Jonathan <88222379+ShonnaDev@users.noreply.github.com>
2025-02-02 21:49:43 -0600

    Se agrego lista de amigos a app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array donde se almacenarán los amigos
+var listaDeAmigos = [];
+
+
+
