bbaca233ff6445594e531f9bf2099838e9103ee4
Jonathan <88222379+ShonnaDev@users.noreply.github.com>
2025-02-02 23:08:06 -0600

    se agrego la funcion mostrarAmigos

--- app.js
+++ app.js
@@ -8,10 +8,36 @@ function agregarAmigo(){
     let nombre = document.getElementById("amigo").value;
     if (nombre === "") {
         alert("Debe ingresar un nombre");
-        return false;
+        return
     }
     // Agregar un amigo a la lista
     listaDeAmigos.push(nombre);
     document.getElementById("amigo").value = "";
-    return listaDeAmigos;
+    mostrarAmigos();
+}
+
+function mostrarAmigos(){
+    // Mostrar la lista de amigos
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++){
+        let fila = "<li>" + listaDeAmigos[i] + "</li>";
+        lista.innerHTML += fila;
+    }
+}
+
+
+function sortearAmigo(){
+    // Revisar si la lista de amigos está vacía
+    if (listaDeAmigos.length === 0) {
+        alert("No hay amigos en la lista");
+        return;
+    }
+
+    // Sortear un amigo
+
+    // Generar un número aleatorio entre 0 y la cantidad de amigos en la lista
+    let numeroAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+
+    return listaDeAmigos[numeroAleatorio];
 }
